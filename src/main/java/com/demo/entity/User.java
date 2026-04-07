956d2af448c604834068aeef6f5b6878478309ca
WangRuiming <23302010017@m.fudan.edu.cn>
2026-04-07 17:23:51 +0800

    Initiialize repository

---
+++ src/main/java/com/demo/entity/User.java
@@ -0,0 +1,33 @@
+package com.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String userID;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    private String password;
+
+    private String email;
+
+    private String phone;
+
+    private int isadmin;
+
+    private String picture;
+}
